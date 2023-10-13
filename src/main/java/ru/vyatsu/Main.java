c64fb6fde68a822d4330f88855dc914d0fb7073f
Корчемкин Андрей <aak25096@gmail.com>
2023-10-13 16:36:42 +0300

    Update main class

--- src/main/java/ru/vyatsu/Main.java
+++ src/main/java/ru/vyatsu/Main.java
@@ -5,6 +5,8 @@ import ru.vyatsu.service.Parser;
 import ru.vyatsu.service.ParserFactory;
 import ru.vyatsu.service.converters.XMLtoJSONTransformer;
 import ru.vyatsu.service.structure.Brand;
+import ru.vyatsu.service.structure.BrandWrapper;
+import ru.vyatsu.service.structure.Brands;
 import ru.vyatsu.service.structure.GarageXML;
 
 import java.io.IOException;
@@ -12,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -23,7 +26,18 @@ public class Main {
             GarageXML garageXML = (GarageXML) xmlParser.parse(xmlContent);
 
             XMLtoJSONTransformer transformer = new XMLtoJSONTransformer();
-            List<Brand> brands = transformer.transform(garageXML);
+            List<Brand> brandList = transformer.transform(garageXML);
+
+            // Обернуть список брендов
+            List<BrandWrapper> brandWrappers = new ArrayList<>();
+            for (Brand brand : brandList) {
+                BrandWrapper brandWrapper = new BrandWrapper();
+                brandWrapper.setBrand(brand);
+                brandWrappers.add(brandWrapper);
+            }
+
+            Brands brands = new Brands();
+            brands.setBrands(brandWrappers);
 
             ObjectMapper objectMapper = new ObjectMapper();
             String jsonContent = objectMapper.writeValueAsString(brands);
@@ -36,3 +50,4 @@ public class Main {
         }
     }
 }
+
