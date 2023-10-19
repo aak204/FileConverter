a795e1a6bdb5e8f29d4b10b397adf6ecd57256fd
Корчемкин Андрей <aak25096@gmail.com>
2023-10-19 14:53:22 +0300

    Refactor

--- src/main/java/ru/vyatsu/service/converters/JSONtoXMLTransformer.java
+++ src/main/java/ru/vyatsu/service/converters/JSONtoXMLTransformer.java
@@ -1,4 +1,31 @@
 package ru.vyatsu.service.converters;
 
+import ru.vyatsu.service.structure.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JSONtoXMLTransformer {
+
+    public GarageXML transform(Brands brandsJSON) {
+        GarageXML garageXML = new GarageXML();
+        List<CarXML> carXMLList = new ArrayList<>();
+
+        for (BrandWrapper brandWrapper : brandsJSON.getBrands()) {
+            Brand brand = brandWrapper.getBrand();
+            for (CarJSON car : brand.getCars()) {
+                CarXML carXML = new CarXML();
+                carXML.setBrand(brand.getName());
+                carXML.setModel(car.getModel());
+                carXML.setYear(car.getYear());
+                carXML.setColor(car.getColor());
+                carXML.setEngine(car.getEngine());
+
+                carXMLList.add(carXML);
+            }
+        }
+
+        garageXML.setCars(carXMLList);
+        return garageXML;
+    }
 }
