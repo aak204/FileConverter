810da894e431e09efc831b6b22290827473b769c
Корчемкин Андрей <aak25096@gmail.com>
2023-10-19 21:46:50 +0300

    BugFix v3

--- src/main/java/ru/vyatsu/Main.java
+++ src/main/java/ru/vyatsu/Main.java
@@ -18,48 +18,65 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 public class Main {
     public static void main(String[] args) throws IOException {
-        String filePath = "src\\main\\resources\\data.xml";
-        String xmlContent = Files.readString(Paths.get(filePath), StandardCharsets.UTF_8);
-        try {
-            Parser xmlParser = ParserFactory.createParser("XML");
-            GarageXML garageXML = (GarageXML) xmlParser.parse(xmlContent);
+        if (args.length < 2) {
+            System.out.println("Нужно ввести минимум 2 аргумента: [input file path] [name output file]");
+        }
+        else {
+            String filePath = args[0];
+            String[] fileResolutionArg1 = args[0].split("\\.");
+            String[] fileResolutionArg2 = args[1].split("\\.");
+            String content = Files.readString(Paths.get(filePath), StandardCharsets.UTF_8);
+            if(fileResolutionArg1[1].equals("xml") && fileResolutionArg2[1].equals("json")){
+                try {
+                    Parser xmlParser = ParserFactory.createParser("XML");
+                    GarageXML garageXML = (GarageXML) xmlParser.parse(content);
 
-            XMLtoJSONTransformer transformer = new XMLtoJSONTransformer();
-            List<Brand> brandList = transformer.transform(garageXML);
+                    XMLtoJSONTransformer transformer = new XMLtoJSONTransformer();
+                    List<Brand> brandList = transformer.transform(garageXML);
 
-            // Обернуть список брендов
-            List<BrandWrapper> brandWrappers = new ArrayList<>();
-            for (Brand brand : brandList) {
-                BrandWrapper brandWrapper = new BrandWrapper();
-                brandWrapper.setBrand(brand);
-                brandWrappers.add(brandWrapper);
+                    // Обернуть список брендов
+                    List<BrandWrapper> brandWrappers = new ArrayList<>();
+                    for (Brand brand : brandList) {
+                        BrandWrapper brandWrapper = new BrandWrapper();
+                        brandWrapper.setBrand(brand);
+                        brandWrappers.add(brandWrapper);
+                    }
+
+                    Brands brands = new Brands();
+                    brands.setBrands(brandWrappers);
+
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    String jsonContent = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(brands);
+
+                    Path outputPath = Paths.get(args[1]);
+                    Files.writeString(outputPath, jsonContent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+            else if (fileResolutionArg1[1].equals("json") && fileResolutionArg2[1].equals("xml")) {
+                try{
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    Brands brands = objectMapper.readValue(content, Brands.class);
+                    JSONtoXMLTransformer transformerToXML = new JSONtoXMLTransformer();
+                    GarageXML garageForXML = transformerToXML.transform(brands); // brands здесь - это объект, представляющий JSON
 
-            Brands brands = new Brands();
-            brands.setBrands(brandWrappers);
+                    XmlMapper xmlMapper = new XmlMapper();
+                    xmlMapper.configure( ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true );
+                    String resultXmlContent = xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(garageForXML);
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonContent = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(brands);
+                    Path xmlOutputPath = Paths.get(args[1]);
+                    Files.writeString(xmlOutputPath, resultXmlContent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
-            Path outputPath = Paths.get("new_data.json");
-            Files.writeString(outputPath, jsonContent);
-
-            JSONtoXMLTransformer transformerToXML = new JSONtoXMLTransformer();
-            GarageXML garageForXML = transformerToXML.transform(brands); // brands здесь - это объект, представляющий JSON
-
-            XmlMapper xmlMapper = new XmlMapper();
-            xmlMapper.configure( ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true );
-            String resultXmlContent = xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(garageForXML);
-
-            Path xmlOutputPath = Paths.get("output_data.xml");
-            Files.writeString(xmlOutputPath, resultXmlContent);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/ru/vyatsu/service/structure/EngineXML.java
+++ src/main/java/ru/vyatsu/service/structure/EngineXML.java
@@ -1,16 +1,5 @@
 package ru.vyatsu.service.structure;
 
-public class EngineXML {
-    private String type;
-    private String volume;
-    private String power;
-
-    // Геттеры и сеттеры
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
-    public String getVolume() { return volume; }
-    public void setVolume(String volume) { this.volume = volume; }
-    public String getPower() { return power; }
-    public void setPower(String power) { this.power = power; }
+public record EngineXML(String type, String volume, String power) {
 }
 
